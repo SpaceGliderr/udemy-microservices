c7d0407b836387cdb3836d20d33c2e0ffacebad8
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-03-05 14:48:27 +0800

    Add Query service

---
+++ query/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 4002;
+
+// The body parser is used to appropriately parse content of the users JSON request to show properly in the req.body object
+app.use(bodyParser.json());
+// Enable CORS
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (_, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
+
+app.listen(port, () => {
+  console.log(`Post server listening on port ${port}`);
+});
