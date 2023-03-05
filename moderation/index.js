c7b8f9f9b5995f4e1c274dcd156985659ef8c1db
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-03-05 15:44:46 +0800

    Add Moderation service

---
+++ moderation/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+const port = 4003;
+
+// The body parser is used to appropriately parse content of the users JSON request to show properly in the req.body object
+app.use(bodyParser.json());
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(port, () => {
+  console.log(`Post server listening on port ${port}`);
+});
