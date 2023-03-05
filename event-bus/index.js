ca733087a6b80d514bcf4ceec06a1382b614e133
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-03-05 14:47:55 +0800

    Add Event Bus service

---
+++ event-bus/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 4005;
+
+// The body parser is used to appropriately parse content of the users JSON request to show properly in the req.body object
+app.use(bodyParser.json());
+
+const microservicePorts = ["4000", "4001", "4002"];
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  microservicePorts.forEach((port) => {
+    axios.post(`http://localhost:${port}/events`, event);
+  });
+
+  res.send({ status: "OK" });
+});
+
+app.listen(port, () => {
+  console.log(`Post server listening on port ${port}`);
+});
